354580d3c5f32d55c3a1c64e2b4178f0fdf99d77
DevBack <heltonlima.ti@live.com>
2022-11-30 18:11:10 -0300

    ExercicioPagamento

--- src/struturasequencial/ExercicioPagamento.java
+++ src/struturasequencial/ExercicioPagamento.java
@@ -1,10 +1,36 @@
 package struturasequencial;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * 
+ * Fazer um programa para ler o nome de um(a) funcionário(a), o valor que ele(a) recebe por hora, e a quantidade de horas trabalhadas por ele(a). 
+ * Ao final, mostrar o valor do pagamento do funcionário com uma mensagem explicativa.
+ *
+ */
+
 public class ExercicioPagamento {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		
+		String nome;
+		double valorPorHora;
+		int horasTrabalhadas;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o Nome do Funcionário:");
+		nome = scanner.nextLine();
+		System.out.println("Digite o Valor da Hora de Trabalho do Funcionário:");
+		valorPorHora = scanner.nextDouble();
+		System.out.println("Digite Quantas Horas o Funcionário Trabalhou:");
+		horasTrabalhadas = scanner.nextInt();
+		
+		scanner.close();
+		
+		System.out.printf("O Pagamento para %s Deve ser R$ %.2f .", nome, horasTrabalhadas*valorPorHora );
 	}
-
 }
